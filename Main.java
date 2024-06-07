2fa67277ed681c5af563ce86a2321826394d845f
pragadesh20t@gmail.com <pragadesh20t@gmail.com>
2024-06-07 11:08:18 +0530

    My Project

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 30;
+
+        float result = a + b;
+        System.out.println(result);
+    }
+}
