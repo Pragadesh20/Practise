311fc20114564bf7446b31567dab8588936f81b9
pragadesh20t@gmail.com <pragadesh20t@gmail.com>
2024-06-07 11:11:53 +0530

    Changing A value

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main{
     public static void main(String[] args) {
-        int a = 20;
+        int a = 200;
         int b = 30;
         int c = 40;
         float result = a + b + c;
